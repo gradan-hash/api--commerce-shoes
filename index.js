6dd4c0bf39bfae23b862a076c2a8eccfbbc3cfbb
gradan-hash <mutukunyaa@gmail.com>
2023-08-10 14:27:42 +0300

    origin

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://client-shoes-commerce.vercel.app",
+    ],
     credentials: true,
   })
 );
